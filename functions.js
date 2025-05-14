0fd7877dd39f24c9ecda7f73176e117f904ac5ee
Adwithr <154674132+Adwithr@users.noreply.github.com>
2025-05-15 00:38:52 +0530

    calculate tests and functions

--- functions.js
+++ functions.js
@@ -11,3 +11,12 @@ export function reverseString(str) {
   }
   return str.split("").reverse().join("");
 }
+
+export const calculator = (() => {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const divide = (a, b) => a / b;
+  const multiply = (a, b) => a * b;
+
+  return { add, subtract, divide, multiply };
+})();
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./functions.js";
+import { capitalize, reverseString, calculator } from "./functions.js";
 
 test("first character of a string is capitalized", () => {
   expect(() => capitalize(2)).toThrow();
@@ -10,4 +10,11 @@ test("reverse a string", () => {
   expect(() => capitalize(2)).toThrow();
   expect(() => capitalize()).toThrow();
   expect(reverseString("hello")).toBe("olleh");
+});
+
+test("calculator", () => {
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.subtract(3, 1)).toBe(2);
+  expect(calculator.divide(10, 2)).toBe(5);
+  expect(calculator.multiply(4, 5)).toBe(20);
 });
